db7f2421303e274922bb600701935d8717b6a5ec
Astragor <101702563+Astrikkk@users.noreply.github.com>
2024-10-09 11:44:28 +0200

    start with php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+$number = 12;
+
+
+
+echo  "ur num " , $number . "<br/>";
+
+echo "Asdas" . "<br/>";
+
+$firstNum = 5;
+$secondNum = "3";
+
+function sum(int $a, int $b){
+    return $a+$b;
+}
+
+echo sum($firstNum,$secondNum);
+
+
+?>
